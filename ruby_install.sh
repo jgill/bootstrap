231d163f19f9e9c6fd2c9722ffe2765d76602f4b
John Gill <john.p.gill@gmail.com>
2022-03-18 23:40:47 -0500

    Update local env

--- ruby_install.sh
+++ ruby_install.sh
@@ -1,17 +1,8 @@
-#/usr/local/bin/rbenv install 2.5.1
-rbenv global 2.5.1
+#/usr/local/bin/rbenv install 2.6.6
+rbenv global 2.6.6
 /usr/local/bin/rbenv rehash
 
 /usr/bin/sudo /usr/bin/gem install rake
 /usr/bin/sudo /usr/bin/gem install bundler
 /usr/bin/sudo /usr/bin/gem install tmuxinator
-
-
-#/usr/local/bin/rbenv install 2.4.4
-#rbenv global 2.4.4
-#/usr/local/bin/rbenv rehash
-
-#/usr/bin/sudo /usr/bin/gem install rake
-#/usr/bin/sudo /usr/bin/gem install bundler
-#/usr/bin/sudo /usr/bin/gem install tmuxinator
 
